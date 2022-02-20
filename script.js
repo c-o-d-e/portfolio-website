879df9525f7be46d5e34c98da55f375c013fb694
c-o-d-e <swapyuk@gmail.com>
2022-02-20 22:02:17 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function hideEye() {
+  $("#eye").css("display", "none");
+  document.getElementById("namedata").style.display = "flex";
+  document.getElementById("namedata").style.animation = "animate 0.5s 1";
+  document.getElementById("navbar").style.display = "flex";
+  document.getElementsByClassName("navlist").style.display = flex;
+  document.getElementByTagName("footer").style.display = "flex";
+}
