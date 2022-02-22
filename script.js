d2cafcdadf72d56abf226144a86ba27538adcaa0
c-o-d-e <swapyuk@gmail.com>
2022-02-22 11:46:11 +0000

    comment out unwanted codes

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function hideEye() {
   $("#eye").css("display", "none");
   document.getElementById("namedata").style.display = "flex";
   document.getElementById("namedata").style.animation = "animate 0.5s 1";
-  document.getElementById("navbar").style.display = "flex";
-  document.getElementsByClassName("navlist").style.display = flex;
-  document.getElementByTagName("footer").style.display = "flex";
+  // document.getElementById("navbar").style.display = "flex";
+  // document.getElementsByClassName("navlist").style.display = flex;
+  // document.getElementByTagName("footer").style.display = "flex";
 }
 
 var slideIndex = 1;
