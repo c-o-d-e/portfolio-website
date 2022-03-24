39bdf1fb1e635dedf0ef09ce1bc5dff9fb310edd
c-o-d-e <swapyuk@gmail.com>
2022-03-24 16:30:28 +0000

    commented out eye style and animation

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-function hideEye() {
-  $("#eye").css("display", "none");
-  document.getElementById("namedata").style.display = "flex";
-  document.getElementById("namedata").style.animation = "animate 0.5s 1";
-  // document.getElementById("navbar").style.display = "flex";
-  // document.getElementsByClassName("navlist").style.display = flex;
-  // document.getElementByTagName("footer").style.display = "flex";
-}
+// function hideEye() {
+//   $("#eye").css("display", "none");
+//   document.getElementById("namedata").style.display = "flex";
+//   document.getElementById("namedata").style.animation = "animate 0.5s 1";
+// }
 
 //Navigation highlights onscroll and onclick
 const buttons = document.querySelectorAll(".button-atag");
